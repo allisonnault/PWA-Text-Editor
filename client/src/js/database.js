84abfa86e0a97d7bd7efcd89ecdb21c19c6de7a8
allisonnault <allisonrnault@gmail.com>
2023-03-24 15:44:29 -0500

    app can be downloaded, database not working

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,17 +13,30 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
+
 export const putDb = async (content) => {
-  console.log('Post to the ase');
+  console.log("add to the database");
   const jateDB = await openDB('jate', 1);
   const tx = jateDB.transaction('jate', 'readwrite');
     const store = tx.objectStore('jate');
-    const request = store.add(content);
+    const request = store.put(content);
     const result = await request;
+    console.log('🚀 - data saved to the database', result);
 }
-console.error('putDb not implemented');
+// console.error('putDb not implemented');
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log("Get from the database");
+  const jate = await openDB('jate', 1);
+    const tx = jateDB.transaction('jate', 'readonly');
+    const store = tx.objectStore('jate');
+    const request = store.getAll();
+    const result = await request;
+    console.log('result.value', result);
+    return result;
+};
+
+// console.error('getDb not implemented');
 
 initdb();
