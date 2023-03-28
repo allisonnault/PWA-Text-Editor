f28bfc3f690f031fa5bac33701d999cb23328c31
allisonnault <allisonrnault@gmail.com>
2023-03-28 09:58:01 -0500

    few final edits

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,7 +12,6 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
 
 export const putDb = async (content) => {
   console.log("add to the database");
@@ -23,9 +22,7 @@ export const putDb = async (content) => {
     const result = await request;
     console.log('🚀 - data saved to the database', result);
 }
-// console.error('putDb not implemented');
 
-// TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.log("Get from the database");
   const jateDB = await openDB('jate', 1);
@@ -36,7 +33,5 @@ export const getDb = async () => {
     console.log('result.value', result);
     return result.value;
 };
-
-// console.error('getDb not implemented');
 
 initdb();
